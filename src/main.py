2a4d5c39582a69de8f1a32829dbd4d82bb582e73
YouJin Jung <jungyoujin0527@gmail.com>
2021-12-04 21:43:40 +0900

    Remove unnecessary codes

--- src/main.py
+++ src/main.py
@@ -23,17 +23,3 @@ def main():
 
 if __name__ == "__main__":
   main()
-
-api_url = "https://api.imgflip.com/get_memes"
-get_joke = requests.get(api_url)
-joke_obj = get_joke.json()
-joke = joke_obj["data"]["memes"]
-
-def select_random_joke(joke):
-    return joke[random.randint(0,len(joke)+1)]["url"]
-    
-joke_url = select_random_joke(joke)
-
-print("Hey Justin!! 이거 확인해봐  ")
-print(joke_url)
-print(f"::set-output name=joke::{joke_url}")
